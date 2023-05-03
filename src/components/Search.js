f5d4349a4487048f6ec902571dc48cd61706a37a
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2023-05-03 15:11:22 +0530

    design modified

--- src/components/Search.js
+++ src/components/Search.js
@@ -47,34 +47,30 @@ const Search = () => {
 
   return (
     <>
-        <div className="row" >
-          <div className="col ">
-            <div className="card text-white text-center border-0">
-              <div className="d-flex flex-column align-items-center justify-content-start card-img-overlay ">
-                <form onSubmit={fetchWeather}>
-                  <div className="input-group mb-4 w-100 mx-auto ">
-                    <input type="search" className="form-control position-sticky " placeholder="Enter location" aria-label="Enteer location" aria-describedby="basic-addon2" name='search' value={location} onChange={(e) => setLocation(e.target.value)}/>
-                    <button type="submit" className="input-group-text" id="basic-addon2">
-                      <i className="fas fa-search"></i>
-                    </button>
-                  </div>
-                </form>
-                { data.main ? 
-                  ( <div className="bg-dark bg-opacity-50 px-4 py-3">
-                      <h2 className="card-title mt-0">{data.name}</h2>               
-                      {data.main ? <p className="card-text lead">{date} ,{time} </p> :null }
-                      {emoji ? <img src={`http://openweathermap.org/img/w/${emoji}.png`} className="w-50 h-25"/> :null}
-                      {temp ? <h1>{temp} &deg;C</h1> :null}
-                      {data.weather ? <p className="lead fw-bolder mb-1"> {data.weather[0].main}</p> :null}
-                      {temp_min ? <span className="card-text lead">{temp_min} &deg;C{" "} to{" "}</span> :null}
-                      {temp_max ? <span className="card-text lead">{temp_max} &deg;C </span> :null}
-                    </div>
-                  ) :null
-                }
-              </div>
+      <div className="card text-white text-center border-0">
+        <div className="d-flex flex-column align-items-center justify-content-start card-img-overlay ">
+          <form onSubmit={fetchWeather}>
+            <div className="input-group mb-4 w-100 mx-auto ">
+              <input type="search" className="form-control position-sticky " placeholder="Enter location" aria-label="Enteer location" aria-describedby="basic-addon2" name='search' value={location} onChange={(e) => setLocation(e.target.value)}/>
+              <button type="submit" className="input-group-text" id="basic-addon2">
+                <i className="fas fa-search"></i>
+              </button>
             </div>
-          </div>
+          </form>
+          { data.main ? 
+            ( <div className="bg-dark bg-opacity-50 px-4 py-2">
+                <h2 className="card-title mt-3">{data.name}</h2>               
+                {data.main ? <p className="card-text lead">{date} ,{time} </p> :null }
+                {emoji ? <img src={`http://openweathermap.org/img/w/${emoji}.png`} className="w-50 h-25"/> :null}
+                {temp ? <h1>{temp} &deg;C</h1> :null}
+                {data.weather ? <p className="lead fw-bolder mb-1"> {data.weather[0].main}</p> :null}
+                {temp_min ? <span className="card-text lead">{temp_min} &deg;C{" "} to{" "}</span> :null}
+                {temp_max ? <span className="card-text lead">{temp_max} &deg;C </span> :null}
+              </div>
+            ) :null
+          }
         </div>
+      </div>
     </>
   );
 };
